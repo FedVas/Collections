fc89fc1bd95bfe0a45af326bf41e126a2fbb09f8
Fedor <atlz89@mail.ru>
2023-07-07 22:43:08 +0300

    Add code

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,75 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GameTest {
+
+
+    Player player1 = new Player(6, 31, "Vasya");
+    Player player2 = new Player(3, 30, "Oleg");
+    Player player3 = new Player(5, 28, "Sasha");
+    Player player4 = new Player(8, 31, "Vanya");
+
+    Game game = new Game();
+
+    @Test
+    public void firstPlayerWin() {
+
+        game.register(player1);
+        game.register(player2);
+
+        int expected = 1;
+        int actual = game.round("Vasya", "Oleg");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void secondPlayerWin() {
+
+        game.register(player3);
+        game.register(player4);
+
+        int expected = 2;
+        int actual = game.round("Sasha", "Vanya");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void draw() {
+
+        game.register(player1);
+        game.register(player4);
+
+        int expected = 0;
+        int actual = game.round("Vasya", "Vanya");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void firstPlayerNotFound() {
+
+        game.register(player2);
+        game.register(player3);
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                () -> game.round("Alexey", "Sasha")
+        );
+    }
+
+    @Test
+    public void secondPlayerNotFound() {
+
+        game.register(player2);
+        game.register(player3);
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                () -> game.round("Oleg", "Tolya")
+        );
+    }
+}
